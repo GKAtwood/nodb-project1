c8b9ea5b6e6d18528aa36d5d24c5f6dc5285ea2e
GKAtwood <gabrielle.k85@gmail.com>
2020-06-28 22:34:17 -0600

    ok

--- server/ghosts.js
+++ server/ghosts.js
@@ -3,18 +3,17 @@ let ghosts = [
     {
       id: 1, // DO NOT USE 0 AS ID
       name: "Casper",
-      image: "https://i.ytimg.com/vi/rZjxmGxNuz8/hqdefault.jpg",
+      image: "https://seeklogo.com/images/C/casper-the-friendly-ghost-logo-6BF6BC815F-seeklogo.com.png",
     },
     {
       id: 2,
       name: "Slimer",
-      image:
-        "https://www.shortlist.com/media/imager/201905/14756-posts.article_md.jpg",
+      image:"https://vignette.wikia.nocookie.net/ghostbusters/images/b/bf/SlimergbII720p002.png/revision/latest/top-crop/width/220/height/220?cb=20121005091456",
     },
     {
       id: 3,
       name: "Beetlejuice",
-      image: "https://pmcvariety.files.wordpress.com/2014/02/beetlejuice.jpg?w=681&h=383&crop=1",
+      image: "https://i1.wp.com/www.the730review.co.uk/wp-content/uploads/2018/03/Beetlejuice_Keaton_3_0.jpg?resize=418%2C315&ssl=1",
     },
     {
       id: 4,
